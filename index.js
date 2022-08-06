3d7d75dbb09fcec76115f1965e57a1820fad064a
George Njogu <sbeveme@gmail.com>
2022-08-06 11:11:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+
+    function clickAlert(){
+        alert('I\'ve been clicked!');
+    }
+
+button.addEventListener('click', clickAlert);
+
 }
+
+addingEventListener();
